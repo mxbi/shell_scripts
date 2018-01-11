f058a94d3b9cef642e7734223495556a65088669
Mikel Bober-Irizar <mikel@mxbi.net>
2018-01-11 23:32:06 +0000

    Add mac support to get_latest_anaconda.sh

--- get_latest_anaconda.sh
+++ get_latest_anaconda.sh
@@ -5,12 +5,27 @@ echo -e "${BLUE}Installing Anaconda3. https://github.com/mxbi/shell_scripts/ ${N
 
 REPO="https://repo.continuum.io/archive/"
 
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Detected MacOS"
+    RC=".bash_profile"
+    FILENAME="$(curl -s ${REPO} | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '[^/"]+' | grep MacOSX-x86_64.sh | grep Anaconda3 | head -n 1)" # Do something under Mac OS X platform
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Detected Linux"
+    RC=".bashrc"
+    FILENAME="$(curl -s ${REPO} | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '[^/"]+' | grep Linux-x86_64.sh | grep Anaconda3 | head -n 1)"  # Do something under GNU/Linux platform
+fi
+
 # Get filename of latest Anaconda
-FILENAME="$(wget -qO- ${REPO} | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '[^/"]+' | grep Linux-x86_64.sh | grep Anaconda3 | head -n 1)" &&
 DL_URL=$REPO$FILENAME &&
 
 echo -e "${BLUE}Downloading $DL_URL${NC}" &&
-wget $DL_URL -O $FILENAME &&
+if [ -x "$(command -v wget)" ]
+then
+    wget $DL_URL -O $FILENAME
+else
+    curl $DL_URL > $FILENAME
+fi &&
+
 chmod u+x $FILENAME &&
 
 echo -e "${BLUE}Installing $FILENAME${NC}" &&
@@ -19,13 +34,13 @@ echo -e "${BLUE}Installing $FILENAME${NC}" &&
 echo -e "${BLUE}Deleting anaconda file${NC}" &&
 rm $FILENAME &&
 
-echo -e "${BLUE}Adding to .bashrc${NC}" &&
-echo '' >> $HOME/.bashrc &&
-echo '# Anaconda3 installation' >> $HOME/.bashrc &&
-echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> $HOME/.bashrc &&
+echo -e "${BLUE}Adding to ${RC}${NC}" &&
+echo '' >> $HOME/${RC} &&
+echo '# Anaconda3 installation' >> $HOME/${RC} &&
+echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> $HOME/${RC} &&
 
-echo -e "${BLUE}Sourcing .bashrc${NC}" &&
-source $HOME/.bashrc &&
+echo -e "${BLUE}Sourcing ${RC}${NC}" &&
+source $HOME/${RC} &&
 
 echo -e "${BLUE}DONE! :)${NC}" &&
 python --version
