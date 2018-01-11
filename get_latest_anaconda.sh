97c409b3faeb638e6aa56ed61919607998ca42ac
Mikel Bober-Irizar <mikel@mxbi.net>
2018-01-11 21:23:38 +0000

    Update get_latest_anaconda.sh

--- get_latest_anaconda.sh
+++ get_latest_anaconda.sh
@@ -1,26 +1,28 @@
+echo "Installing Anaconda3. https://github.com/mxbi/shell_scripts/"
+
 REPO="https://repo.continuum.io/archive/"
 
 # Get filename of latest Anaconda
-FILENAME="$(wget -qO- ${REPO} | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '[^/"]+' | grep Linux-x86_64.sh | grep Anaconda3 | head -n 1)"
-DL_URL=$REPO$FILENAME
+FILENAME="$(wget -qO- ${REPO} | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '[^/"]+' | grep Linux-x86_64.sh | grep Anaconda3 | head -n 1)" &&
+DL_URL=$REPO$FILENAME &&
 
 echo "Downloading $DL_URL"
-wget $DL_URL
-chmod u+x $FILENAME
+wget $DL_URL &&
+chmod u+x $FILENAME &&
 
 echo "Installing $FILENAME"
-./$FILENAME -b
+./$FILENAME -b &&
 
 echo "Deleting anaconda file"
-rm $FILENAME
+rm $FILENAME &&
 
 echo "Adding to .bashrc"
-echo '' >> $HOME/.bashrc
-echo '# Anaconda3 installation' >> $HOME/.bashrc
-echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> $HOME/.bashrc
+echo '' >> $HOME/.bashrc &&
+echo '# Anaconda3 installation' >> $HOME/.bashrc &&
+echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> $HOME/.bashrc &&
 
 echo "Sourcing .bashrc"
-source $HOME/.bashrc
+source $HOME/.bashrc &&
 
 echo "DONE! :)"
 python --version
