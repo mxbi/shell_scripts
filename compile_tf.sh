59790cd1b6a6ba0e906d950e026e05f8d61c0b5d
Mikel Bober-Irizar <mikel@mxbi.net>
2018-02-12 13:38:31 +0000

    Update compile_tf.sh to TensorFlow 1.5.0

--- compile_tf.sh
+++ compile_tf.sh
@@ -1,28 +1,40 @@
+echo "[STATUS] Cloning TF repo"
+
 git clone https://github.com/tensorflow/tensorflow.git &&
 
-#sudo apt install openjdk-8-jdk
-#yes | sudo add-apt-repository ppa:webupd8team/java
-#sudo apt-get update && sudo apt-get install oracle-java8-installer
+echo "[STATUS] Installing Java"
+
+sudo apt install -y openjdk-8-jdk
+yes | sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update && sudo apt-get -y install oracle-java8-installer
+
+echo "[STATUS] Installing other dependencies"
 
 sudo apt-get install -y libcupti-dev  &&
 sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip python &&
+
+echo "[STATUS] Installing Bazel 0.8.1"
 
 wget https://github.com/bazelbuild/bazel/releases/download/0.8.1/bazel_0.8.1-linux-x86_64.deb -O bazel.deb &&
 sudo dpkg -i bazel.deb &&
 rm bazel.deb &&
 
 cd tensorflow &&
-git checkout v1.4.1 &&
+git checkout v1.5.0 &&
 
-echo "!!!!!!!!! TENSORFLOW CONFIGURING:"
+echo "[STATUS] !!!!!!!!! TENSORFLOW CONFIGURING:"
 # Build with cuda support => y
 # Cuda version => 9.0
 # CuDNN version => 7.0.5
 # All other configure options default (this will vary depending on your system).
 ./configure &&
 
+echo "[STATUS] Building Tensorflow!"
+
 bazel clean &&
 bazel build --config=mkl --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package &&
+
+echo "[STATUS] Installing TF Package"
 
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg &&
 pip install /tmp/tensorflow_pkg/*.whl &&
