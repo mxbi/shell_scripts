6b66a71fd85950a7598f6dae945fef50d63f0a5c
Mikel Bober-Irizar <mikel@mxbi.net>
2018-03-24 18:51:38 +0000

    Add cmake to apt_essentials

--- apt_essentials.sh
+++ apt_essentials.sh
@@ -1,3 +1,3 @@
-sudo apt-get install -y git build-essential sshfs nmon htop pigz p7zip-full pwgen &&
+sudo apt-get install -y git build-essential sshfs nmon htop pigz p7zip-full pwgen cmake &&
 
 echo "Done! :)"
