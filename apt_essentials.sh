941a1b1b9e39b2ef002dc736f4213a8c502b8894
Mikel Bober-Irizar <mikel@mxbi.net>
2019-11-04 12:13:58 +0000

    Update apt_essentials.sh

--- apt_essentials.sh
+++ apt_essentials.sh
@@ -1,3 +1,3 @@
-sudo apt-get install -y git build-essential sshfs nmon htop pigz p7zip-full pwgen cmake &&
+sudo apt-get install -y git build-essential sshfs nmon htop pigz p7zip-full pwgen cmake screen net-tools curl wget &&
 
 echo "Done! :)"
